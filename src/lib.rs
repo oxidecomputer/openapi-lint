4ec920055b9a751918c50485518b8ad410984d3a
Adam H. Leventhal <ahl@oxide.computer>
2022-01-18 12:29:11 -0800

    fix snake_case rendering of "ipv6"

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,7 @@
 //! generators would have a hard time turning into easy-to-use native
 //! constructs.
 
-use convert_case::{Case, Casing};
+use convert_case::{Boundary, Case, Casing, Converter};
 use indexmap::IndexMap;
 use openapiv3::{Components, OpenAPI, Operation, Parameter, ReferenceOr, Schema, Type};
 
@@ -15,168 +15,192 @@ mod walker;
 use walker::SchemaWalker;
 
 pub fn validate(spec: &OpenAPI) -> Vec<String> {
-    let schema = spec.walk().flat_map(|(name, schema)| {
-        validate_subschemas(spec, schema)
-            .map(|msg| {
-                format!(
-                    "problem with type {}: {}",
-                    name.unwrap_or_else(|| "<unknown>".to_string()),
-                    msg
-                )
-            })
-            .into_iter()
-            .chain(validate_object_camel_case(schema))
-    });
-
-    let operations = spec.operations().filter_map(validate_operation_id);
-    let parameters = spec
-        .operations()
-        .flat_map(|(_, _, op)| validate_operation_parameters(spec, op));
-    let named_schemas = spec.components.iter().flat_map(|components| {
-        components
-            .schemas
-            .keys()
-            .filter_map(|type_name| validate_named_schema(type_name))
-    });
-
-    schema
-        .chain(operations)
-        .chain(parameters)
-        .chain(named_schemas)
-        .collect()
+    Validator::default().validate(spec)
 }
 
-fn validate_subschemas(spec: &OpenAPI, schema: &Schema) -> Option<String> {
-    let subschemas = subschemas(spec, schema);
-    let mut iter = subschemas.into_iter();
+struct Validator {
+    pub(crate) snakifier: Converter,
+}
 
-    const PRE: &str = "mismatched types between subschemas; this is often \
+impl Default for Validator {
+    fn default() -> Self {
+        Self {
+            snakifier: Converter::new()
+                .remove_boundary(Boundary::LowerDigit)
+                .to_case(Case::Snake),
+        }
+    }
+}
+
+impl Validator {
+    pub fn validate(&self, spec: &OpenAPI) -> Vec<String> {
+        let schema = spec.walk().flat_map(|(name, schema)| {
+            self.validate_subschemas(spec, schema)
+                .map(|msg| {
+                    format!(
+                        "problem with type {}: {}",
+                        name.unwrap_or_else(|| "<unknown>".to_string()),
+                        msg
+                    )
+                })
+                .into_iter()
+                .chain(self.validate_object_camel_case(schema))
+        });
+
+        let operations = spec
+            .operations()
+            .filter_map(|path_method_op| self.validate_operation_id(path_method_op));
+        let parameters = spec
+            .operations()
+            .flat_map(|(_, _, op)| self.validate_operation_parameters(spec, op));
+        let named_schemas = spec.components.iter().flat_map(|components| {
+            components
+                .schemas
+                .keys()
+                .filter_map(|type_name| self.validate_named_schema(type_name))
+        });
+
+        schema
+            .chain(operations)
+            .chain(parameters)
+            .chain(named_schemas)
+            .collect()
+    }
+
+    fn validate_subschemas(&self, spec: &OpenAPI, schema: &Schema) -> Option<String> {
+        let subschemas = self.subschemas(spec, schema);
+        let mut iter = subschemas.into_iter();
+
+        const PRE: &str = "mismatched types between subschemas; this is often \
     due to enums with different data payloads and can be resolved using serde \
     adjacent tagging.";
-    const POST: &str = "For more info, see \
+        const POST: &str = "For more info, see \
     https://github.com/oxidecomputer/openapi-lint#type-mismatch";
 
-    if let Some(first) = iter.next() {
-        for ty in iter {
-            match (first, ty) {
-                (Type::String(_), Type::String(_))
-                | (Type::Number(_), Type::Number(_))
-                | (Type::Integer(_), Type::Integer(_))
-                | (Type::Object(_), Type::Object(_))
-                | (Type::Array(_), Type::Array(_))
-                | (Type::Boolean {}, Type::Boolean {}) => {}
-                (a, b) => {
-                    return Some(format!(
-                        "{}\nthis schema's type\n{:#?}\ndiffers from this\n{:#?}\n\n{}",
-                        PRE, a, b, POST,
-                    ))
+        if let Some(first) = iter.next() {
+            for ty in iter {
+                match (first, ty) {
+                    (Type::String(_), Type::String(_))
+                    | (Type::Number(_), Type::Number(_))
+                    | (Type::Integer(_), Type::Integer(_))
+                    | (Type::Object(_), Type::Object(_))
+                    | (Type::Array(_), Type::Array(_))
+                    | (Type::Boolean {}, Type::Boolean {}) => {}
+                    (a, b) => {
+                        return Some(format!(
+                            "{}\nthis schema's type\n{:#?}\ndiffers from this\n{:#?}\n\n{}",
+                            PRE, a, b, POST,
+                        ))
+                    }
                 }
             }
         }
+
+        None
     }
 
-    None
-}
-
-fn subschemas<'a>(spec: &'a OpenAPI, schema: &'a Schema) -> Vec<&'a Type> {
-    match &schema.schema_kind {
-        openapiv3::SchemaKind::OneOf { one_of: ofs }
-        | openapiv3::SchemaKind::AllOf { all_of: ofs }
-        | openapiv3::SchemaKind::AnyOf { any_of: ofs } => ofs
-            .iter()
-            .flat_map(|subschema| subschemas(spec, subschema.item(&spec.components).unwrap()))
-            .collect(),
-        openapiv3::SchemaKind::Not { .. } => todo!(),
-        openapiv3::SchemaKind::Type(t) => vec![t],
-        openapiv3::SchemaKind::Any(_) => todo!(),
+    fn subschemas<'a>(&self, spec: &'a OpenAPI, schema: &'a Schema) -> Vec<&'a Type> {
+        match &schema.schema_kind {
+            openapiv3::SchemaKind::OneOf { one_of: ofs }
+            | openapiv3::SchemaKind::AllOf { all_of: ofs }
+            | openapiv3::SchemaKind::AnyOf { any_of: ofs } => ofs
+                .iter()
+                .flat_map(|subschema| {
+                    self.subschemas(spec, subschema.item(&spec.components).unwrap())
+                })
+                .collect(),
+            openapiv3::SchemaKind::Not { .. } => todo!(),
+            openapiv3::SchemaKind::Type(t) => vec![t],
+            openapiv3::SchemaKind::Any(_) => todo!(),
+        }
     }
-}
 
-fn validate_object_camel_case(schema: &Schema) -> Vec<String> {
-    let mut ret = Vec::new();
+    fn validate_object_camel_case(&self, schema: &Schema) -> Vec<String> {
+        let mut ret = Vec::new();
 
-    if let openapiv3::SchemaKind::Type(Type::Object(obj)) = &schema.schema_kind {
-        for prop_name in obj.properties.keys() {
-            let snake = prop_name.to_case(Case::Snake);
-            if prop_name.clone() != snake {
-                ret.push(format!(
-                    "an object contains a property '{}' which is not \
+        if let openapiv3::SchemaKind::Type(Type::Object(obj)) = &schema.schema_kind {
+            for prop_name in obj.properties.keys() {
+                let snake = self.snakifier.convert(prop_name);
+                if prop_name.clone() != snake {
+                    ret.push(format!(
+                        "an object contains a property '{}' which is not \
                     snake_case:\n{:#?}\n\
                     Add #[serde(rename = \"{}\")] to the member or \
                     #[serde(rename_all = \"snake_case\")] to the struct.\n\
                     For more info see \
                     https://github.com/oxidecomputer/openapi-lint#naming",
-                    prop_name, schema, snake
-                ))
+                        prop_name, schema, snake
+                    ))
+                }
             }
+        }
+
+        ret
+    }
+
+    fn validate_operation_id(&self, path_method_op: (&str, &str, &Operation)) -> Option<String> {
+        let (path, method, op) = path_method_op;
+
+        const INFO: &str = "For more info, see \
+    https://github.com/oxidecomputer/openapi-lint#naming";
+
+        if let Some(operation_id) = &op.operation_id {
+            let snake = self.snakifier.convert(operation_id);
+            if operation_id.as_str() == snake {
+                return None;
+            }
+            Some(format!(
+                "The operation for {} {} is named \"{}\" which is not snake_case\n{}",
+                path, method, operation_id, INFO,
+            ))
+        } else {
+            Some(format!(
+                "The operation for {} {} does not have an operation_id\n{}",
+                path, method, INFO,
+            ))
         }
     }
 
-    ret
-}
-
-fn validate_operation_id(path_method_op: (&str, &str, &Operation)) -> Option<String> {
-    let (path, method, op) = path_method_op;
-
-    const INFO: &str = "For more info, see \
+    fn validate_operation_parameters(&self, spec: &OpenAPI, op: &Operation) -> Vec<String> {
+        const INFO: &str = "For more info, see \
     https://github.com/oxidecomputer/openapi-lint#naming";
 
-    if let Some(operation_id) = &op.operation_id {
-        let snake = operation_id.to_case(Case::Snake);
-        if operation_id.as_str() == snake {
+        let operation_id = op.operation_id.as_deref().unwrap_or("<unknown>");
+        op.parameters
+            .iter()
+            .filter_map(|ref_or_param| {
+                let param = ref_or_param.item(&spec.components)?;
+
+                let name = &param.parameter_data_ref().name;
+                let snake = self.snakifier.convert(name);
+
+                if name.as_str() != snake {
+                    Some(format!(
+                        "The parameter \"{}\" to {} should be snake_case.\n{}",
+                        name, operation_id, INFO,
+                    ))
+                } else {
+                    None
+                }
+            })
+            .collect()
+    }
+
+    fn validate_named_schema(&self, type_name: &str) -> Option<String> {
+        const INFO: &str = "For more info, see \
+    https://github.com/oxidecomputer/openapi-lint#naming";
+
+        let pascal = type_name.to_case(Case::Pascal);
+        if type_name == pascal {
             return None;
         }
+
         Some(format!(
-            "The operation for {} {} is named \"{}\" which is not snake_case\n{}",
-            path, method, operation_id, INFO,
-        ))
-    } else {
-        Some(format!(
-            "The operation for {} {} does not have an operation_id\n{}",
-            path, method, INFO,
-        ))
-    }
-}
-
-fn validate_operation_parameters(spec: &OpenAPI, op: &Operation) -> Vec<String> {
-    const INFO: &str = "For more info, see \
-    https://github.com/oxidecomputer/openapi-lint#naming";
-
-    let operation_id = op.operation_id.as_deref().unwrap_or("<unknown>");
-    op.parameters
-        .iter()
-        .filter_map(|ref_or_param| {
-            let param = ref_or_param.item(&spec.components)?;
-
-            let name = &param.parameter_data_ref().name;
-            let snake = name.to_case(Case::Snake);
-
-            if name.as_str() != snake {
-                Some(format!(
-                    "The parameter \"{}\" to {} should be snake_case.\n{}",
-                    name, operation_id, INFO,
-                ))
-            } else {
-                None
-            }
-        })
-        .collect()
-}
-
-fn validate_named_schema(type_name: &str) -> Option<String> {
-    const INFO: &str = "For more info, see \
-    https://github.com/oxidecomputer/openapi-lint#naming";
-
-    let pascal = type_name.to_case(Case::Pascal);
-    if type_name == pascal {
-        return None;
-    }
-
-    Some(format!(
-        "The type \"{}\" has a name that is not PascalCase; to rename it add \
+            "The type \"{}\" has a name that is not PascalCase; to rename it add \
         #[serde(rename = \"{}\")]\n{}",
-        type_name, pascal, INFO,
-    ))
+            type_name, pascal, INFO,
+        ))
+    }
 }
 
 trait ReferenceOrExt<T: ComponentLookup> {
@@ -214,7 +238,7 @@ impl ComponentLookup for Schema {
 
 #[cfg(test)]
 mod tests {
-    use crate::validate;
+    use crate::{validate, Validator};
 
     #[test]
     fn bad_schema() {
@@ -222,5 +246,15 @@ mod tests {
 
         let actual = validate(&openapi).join("\n\n");
         expectorate::assert_contents("src/tests/errors.out", &actual);
+    }
+
+    #[test]
+    fn test_ipv6() {
+        let validator = Validator::default();
+        assert_eq!(validator.snakifier.convert("ipv6"), "ipv6");
+        assert_eq!(
+            validator.snakifier.convert("the_ipv6_network"),
+            "the_ipv6_network"
+        );
     }
 }
